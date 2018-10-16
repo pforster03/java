2b2a348c79cef656a0ea9ca890fdb56fb127e4cc
philip.hogan <booah8@gmail.com>
2018-10-16 13:27:08 -0400

    Add null output file

--- Hailstones.java
+++ Hailstones.java
@@ -6,14 +6,18 @@ import java.io.File;
 
 
 public class Hailstones{
+  static BigInteger thisNum;
+  static BigInteger biggest;
+  static BigInteger count;
+
   public static void main(String[] args) {
     if(Arrays.stream(args).anyMatch("--help"::equals)){
       System.out.println(String.join(
         System.getProperty("line.separator"),
         "Usage",
-        "Hailstones -|infile -|outfile",
-        "Computes the hailstones of a file. Pass it with arguments to read the number from a file and write it to the outfile. Use - to prompt for a number, and - to display the numbers to the terminal.",
-        "And the last!"
+        "Hailstones -|infile -|outfile|null",
+        "Computes the hailstones of a number. Pass it with arguments to read the number from a file and write it to the outfile. Use - to prompt for a number, and - to display the numbers to the terminal.",
+        "While it is running, press Enter to show progress. Using null as an outfile removes most output."
       ));
       return;
     }
@@ -45,7 +49,7 @@ public class Hailstones{
     FileWriter fos = null;
 
 
-    if(args.length > 1 && !args[1].equals("-")){
+    if(args.length > 1 && !args[1].equals("-") && !args[1].equals("null")){
       fout = new File(args[1]);
       try{
         fos = new FileWriter(fout);
@@ -62,17 +66,34 @@ public class Hailstones{
     final BigInteger one = BigInteger.valueOf(1);
     final BigInteger two = BigInteger.valueOf(2);
     final BigInteger three = BigInteger.valueOf(3);
-    BigInteger thisNum = startingNum;
-    BigInteger biggest = BigInteger.valueOf(0);
+    thisNum = startingNum;
+    biggest = BigInteger.valueOf(0);
+    count = BigInteger.valueOf(0);
+
+    Thread consoleWatcher = new Thread() {
+      @Override
+      public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while(true){// E
+          scanner.nextLine();
+          System.err.println("Working on " + Hailstones.thisNum + ", iteration #" + Hailstones.count);
+        }
+      }
+    };
+
+    consoleWatcher.start();
+
     while(!thisNum.equals(one)){
       if(thisNum.compareTo(biggest) == 1) biggest = thisNum;//store the biggest number
       if(args.length > 1 && !args[1].equals("-")){
-        try{
-          fos.write(thisNum + System.getProperty("line.separator"));
-          fos.flush();
-        }catch(IOException e){
-          e.printStackTrace();
-          System.exit(2);
+        if(!args[1].equals("null")){
+          try{
+            fos.write(thisNum + System.getProperty("line.separator"));
+            fos.flush();
+          }catch(IOException e){
+            e.printStackTrace();
+            System.exit(2);
+          }
         }
       }else{
         System.out.println(thisNum);
@@ -83,14 +104,20 @@ public class Hailstones{
       }else{//odd
         thisNum = thisNum.multiply(three).add(one);
       }
+
+      count = count.add(BigInteger.valueOf(1));
     }
 
+    consoleWatcher.interrupt();
+
     System.err.println("start: " + startingNum);
-    System.err.println("term count");
+    System.err.println("term count: " + count);
     System.err.println("biggest: " + biggest);
     // avoid memory leak
     if(args.length <= 0){
       scan.close();
     }
+
+    System.exit(0);
   }
 }
